968fc6fa0fe29d441d04e8232c24a112b7dff858
Justin Vos <justinvosnz@gmail.com>
2023-12-20 15:09:45 +0000

    Add put messages

--- src/server.js
+++ src/server.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const path = require('path');
-// const fs = require('node:fs/promises');
 
 const app = express();
 
 const port = 3000
 
 app.get('/', (req, res) => {
-  // const data = await fs.readFile('/Users/joe/test.txt', { encoding: 'utf8' });
   const options = {
     root: path.join(__dirname, '..', 'public'),
-    // dotfiles: 'deny',
-    // headers: {
-    //   'x-timestamp': Date.now(),
-    //   'x-sent': true
-    // }
   };
   res.sendFile('index.html', options);
+});
+
+app.put('/messages', (req, res) => {
+  res.send('<ul><li>Hi</li><li>Hey</li></ul>');
 });
 
 app.listen(port, () => {
